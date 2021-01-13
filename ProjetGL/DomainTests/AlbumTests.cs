46e6281ebe3e52666662576e298c0bec8d379fb3
TangiManeux <tmaneux@ensc.fr>
2021-01-13 12:01:25 +0100

    Premier test DomainTests

---
+++ ProjetGL/DomainTests/AlbumTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain;
+
+namespace DomainTests
+{
+    [TestClass]
+    public class AlbumTests
+    {
+        private string _title;
+        private string _isbn;
+        private string _publisher;
+        private string _description;
+        private List<Author> _authors;
+        private List<Genre> _genres;
+        private Category _category;
+        private Series _series;
+        private Album _album;
+
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            _authors = new List<Author> {new Author("Baptiste Pesquet")};
+            _title = "The JavaScript Way";
+            _isbn = "B07C6CTV6S";
+            _publisher = "GitBooks";
+            _description = "This book aims to be a useful companion for anyone wishing to (re)discover the many facets of JavaScript.";
+            _genres = new List<Genre> { new Genre("Informatique") };
+            _category = new Category("Apprentissage");
+            _series = new Series("BaptistePesquet's Adventures");
+            _album = new Album(_title, _isbn, _publisher, _description, _authors, _genres, _category, _series);
+        }
+        [TestMethod]
+        public void TestAlbumToString()
+        {
+            Assert.AreEqual("The JavaScript Way, Apprentissage, GitBooks", _album.ToString());
+        }
+    }
+}
---
+++ ProjetGL/DomainTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DomainTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DomainTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("67f0d3a1-7fb4-4887-8874-b677fe2c1300")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
