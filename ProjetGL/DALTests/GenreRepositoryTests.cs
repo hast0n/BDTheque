099d2d010ffda9fc0759534b8b5e1149403c99e3
TangiManeux <tmaneux@ensc.fr>
2021-01-14 17:19:13 +0100

    Ajout des tests des classes UserRepository et SeriesRepository

---
+++ ProjetGL/DALTests/GenreRepositoryTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain;
+using DAL.Repositories;
+
+namespace DALTests
+{
+    /// <summary>
+    /// Description résumée pour GenreRepositoryTests
+    /// </summary>
+    [TestClass]
+    public class GenreRepositoryTests
+    {
+        [TestInitialize()]
+        public void Initialyse()
+        {
+            TestRepository.ClearSession();
+            TestRepository.ResetBDD();
+        }
+        [TestMethod]
+        public void SeriesGetAllTest()
+        {
+            var seriesRepository = new SeriesRepository();
+            var allSeries = seriesRepository.GetAll();
+            Assert.AreEqual(4, allSeries.Count);
+            string actualSeries = "";
+            foreach (Series aa in allSeries)
+            {
+                actualSeries += aa.ToString() + ", ";
+            }
+            string expectedSeries = "Les Cryptides, Fullmetal Alchemist, Hunter X Hunter, Kookabura, ";
+            Assert.AreEqual(actualSeries, expectedSeries);
+        }
+        [TestMethod]
+        public void SaveUserTest()
+        {
+            var seriesRepository = new SeriesRepository();
+            var newSeries = new Series("Une nouvelle");
+            seriesRepository.Save(newSeries);
+            var allSeries = seriesRepository.GetAll();
+            Assert.AreEqual(5, allSeries.Count);
+            string actualSeries = "";
+            foreach (Series aa in allSeries)
+            {
+                actualSeries += aa.ToString() + ", ";
+            }
+            string expectedSeries = "Les Cryptides, Fullmetal Alchemist, Hunter X Hunter, Kookabura, Une nouvelle, ";
+            Assert.AreEqual(actualSeries, expectedSeries);
+        }
+    }
+}
---
+++ ProjetGL/DALTests/UserRepositoryTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain;
+using DAL.Repositories;
+
+namespace DALTests
+{
+    [TestClass]
+    public class UserRepositoryTests
+    {
+        [TestInitialize()]
+        public void Initialyse()
+        {
+            TestRepository.ClearSession();
+            TestRepository.ResetBDD();
+        }
+        [TestMethod]
+        public void UserGetAllTest()
+        {
+            var userRepository = new UserRepository();
+            var allUsers = userRepository.GetAll();
+            Assert.AreEqual(3, allUsers.Count);
+            string actualUsers = "";
+            foreach (User aa in allUsers) 
+            {
+                actualUsers+=aa.ToString()+", ";
+            }
+            string expectedUsers = "Martin Devreese, Tangi Maneux, Toto Lambda, ";
+            Assert.AreEqual(actualUsers, expectedUsers);
+        }
+        [TestMethod]
+        public void GetUserTest()
+        {
+            var userRepository = new UserRepository();
+            var user = userRepository.GetUser("mdevreese@ensc.fr", "f4f263e439cf40925e6a412387a9472a6773c2580212a4fb50d224d3a817de17");
+            var actualUser = user.ToString();
+            var expectedUser = "Martin Devreese";
+            Assert.AreEqual(actualUser, expectedUser);
+        }
+        [TestMethod]
+        public void SaveUserTest()
+        {
+            var userRepository = new UserRepository();
+            var newUser = new User("Mathew", "Bold", "mbold@ensc.fr", 0, "f4f263e439cf40925e6a412387a9472a6773c2580212a4fb50d224d3a817de17");
+            userRepository.Save(newUser);
+            var user = userRepository.GetUser("mbold@ensc.fr", "f4f263e439cf40925e6a412387a9472a6773c2580212a4fb50d224d3a817de17");
+            var actualUser = user.ToString();
+            var expectedUser = "Mathew Bold";
+            Assert.AreEqual(actualUser, expectedUser);
+        }
+    }
+}
